b617fa260c21b1ee2f3d6b636b33325ec5d816e9
ernestognw <ernestognw@gmail.com>
2021-09-27 01:11:03 -0500

    Prepared deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -8,4 +9,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.6.6",
+  networks: {
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,8 +3,10 @@ const deploy = async () => {
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const PlatziPunks = await ethers.getContractFactory("Raffle");
-  const deployed = await PlatziPunks.deploy(10000);
+  const Raffle = await ethers.getContractFactory("Raffle");
+  // TODO: Change date
+  const revealDate = Date.now() / 1000; // Test only
+  const deployed = await Raffle.deploy(10000, revealDate.toFixed(0));
 
   console.log("Raffle NFT address:", deployed.address);
 };
